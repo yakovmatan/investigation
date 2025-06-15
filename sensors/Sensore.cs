ad5c93aba0aa3ed7fb149d580a68e14e6f8951f0
yakov matan <yakovmatan@gmail.com>
2025-06-15 21:25:30 +0300

    create class sensor

---
+++ sensors/Sensore.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace investigation.sensors
+{
+    internal abstract class Sensore
+    {
+        protected string type;
+
+        public Sensore(string type)
+        {
+            this.type = type;
+        }
+
+        public void Activate(Dictionary<string, int> sensors)
+        {
+            if (sensors.ContainsKey(this.type))
+            {
+                sensors[this.type] += 1;
+            }
+            else
+            {
+                sensors[this.type] = 1;
+            }
+                
+        }
+    }
+}
