e974ebe9953befdbf4e03c4d30b1946fcd91914e
yakov matan <yakovmatan@gmail.com>
2025-06-15 17:20:32 +0300

    create class agent

---
+++ iranianAgents/Agent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace investigation.iranianAgents
+{
+    internal abstract class Agent
+    {
+        public string type { get; }
+        protected static Random random = new Random();
+        protected string[] weaknessSensors;
+
+        public Agent(int numOfSensors,List<string> availableSensors)
+        {
+            InitializeRandomSensors(numOfSensors, availableSensors);
+        }
+        protected void InitializeRandomSensors(int numOfSensors,List<string> availableSensors)
+        {
+            weaknessSensors = new string[numOfSensors];
+            for (int i = 0; i < numOfSensors; i++)
+            {
+                int index = random.Next(availableSensors.Count);
+                weaknessSensors[i] = availableSensors[index];
+            }    
+        }
+
+    }
+}
