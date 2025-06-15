3170f2fc8e7a094a50a3de9d3565fb3b4cd47c8b
yakov matan <yakovmatan@gmail.com>
2025-06-15 23:42:25 +0300

    create class room

---
+++ room/Room.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace investigation.room
+{
+    internal class Room
+    {
+        public Dictionary<string,int> activeSensore = new Dictionary<string,int>();
+
+        public int NumOfMatches(Dictionary<string,int> weaknessSensors)
+        {
+            int numOfMatches = 0;
+            foreach (var keyValue in activeSensore)
+            {
+                if (weaknessSensors.ContainsKey(keyValue.Key))
+                {
+                    numOfMatches += Math.Min(activeSensore[keyValue.Key], weaknessSensors[keyValue.Key]);
+                }
+            }
+            return numOfMatches;
+        }
+    }
+}
