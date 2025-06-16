12c4a78f9934c02476a4dbe55edac04cd39c1e9d
yakov matan <yakovmatan@gmail.com>
2025-06-16 15:07:14 +0300

    create class pulse sensor

---
+++ sensors/PulseSensor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace investigation.sensors
+{
+    internal class PulseSensor:Sensor
+    {
+        private int count = 0;
+        public PulseSensor():base("pulse")
+        {
+
+        }
+
+        public override void Activate(int index, string[] sensors)
+        {
+            if (count == 3)
+            {
+                Console.WriteLine("The sensor is broken");
+                return;
+            }
+            base.Activate(index, sensors);
+            count++;
+        }
+    }
+}
