72c021fbb731d72fd43d1b515cac4fe1b99ca939
yakov matan <yakovmatan@gmail.com>
2025-06-16 17:10:33 +0300

    create class Squad leader

---
+++ iranianAgents/SquadLeader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace investigation.iranianAgents
+{
+    internal class SquadLeader:Agent
+    {
+        public SquadLeader(List<string> availableSensors):base(4,availableSensors,"squad leader")
+        {
+
+        }
+    }
+}
