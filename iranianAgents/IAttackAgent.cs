0c185e4d6756ca1fa9ea67e3d5b28c3008ec5f44
yakov matan <yakovmatan@gmail.com>
2025-06-17 15:52:15 +0300

    create interface IAttackAgent

---
+++ iranianAgents/IAttackAgent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace investigation.iranianAgents
+{
+    internal interface IAttackAgent
+    {
+        int CounterAttack {get; set;}
+        void Attack();
+    }
+}
