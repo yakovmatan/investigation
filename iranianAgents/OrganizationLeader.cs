66dfe2d0a9fe3a4bc5690e8fbe8aa319488c8175
yakov matan <yakovmatan@gmail.com>
2025-06-17 18:46:22 +0300

    create class organization Leader

---
+++ iranianAgents/OrganizationLeader.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using investigation.sensors;
+
+namespace investigation.iranianAgents
+{
+    internal class OrganizationLeader:Agent,IAttackAgent
+    {
+        private int CounterAttack = 0;
+
+        public OrganizationLeader(List<string> availableSensors):base(8,availableSensors,"organization leader")
+        {
+
+        }
+
+        public void Attack(Sensor[] sensors)
+        {
+            if (CounterAttack % 3 == 0)
+            {
+                if (!sensors.Any(s => s != null)) return;
+                int index;
+                do
+                {
+                    index = random.Next(sensors.Length);
+                }
+                while (sensors[index] == null);
+
+                sensors[index] = null;
+            }
+            else if (CounterAttack % 10 == 0)
+            {
+                for (int i = 0; i < sensors.Length; i++)
+                {
+                    sensors[i] = null;
+                }
+            }
+            CounterAttack++;
+        }
+
+    }
+}
